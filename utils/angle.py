759b1436b85f0d8ba369804ffb3f832eb44cb8e6
Sora Ozawa <soraozawa56@gmail.com>
2022-06-20 14:02:24 +0900

    feat: 姿勢推定をcloneしてきた

---
+++ utils/angle.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def angle(vec1: np.ndarray, vec2: np.ndarray) -> float:
+    """2つのベクトル間の角度[rad]を計算する
+    Args:
+        vec1 (np.ndarray): ベクトル1
+        vec2 (np.ndarray): ベクトル2
+    Returns:
+        float: 2つのベクトル間の角度[rad]
+    """
+    cos_theta = np.dot(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2))
+    return np.arccos(round(cos_theta, 3))
